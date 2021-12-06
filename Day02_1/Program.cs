5c0a0813fb20bdffd2f0e302fee2611341914691
Martin Zikmund <martin@zikmund.dev>
2021-12-06 22:19:21 +0100

    Day 2

---
+++ Day02_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Dictionary<string, (int x, int depth)> directions = new()
+{
+    { "forward", (1, 0) },
+    { "up", (0, -1) },
+    { "down", (0, 1) },
+
+};
+
+(int x, int depth) position = (0, 0);
+while (await Console.In.ReadLineAsync() is { } line)
+{
+    var command = line.Split(' ');
+    var direction = directions[command[0]];
+    var multiple = int.Parse(command[1]);
+    position = (position.x + multiple * direction.x, position.depth + multiple * direction.depth);
+}
+
+await Console.Out.WriteLineAsync((position.x * position.depth).ToString());
---
+++ Day02_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<string, Func<(int x, int depth, int aim), int, (int x, int depth, int aim)>> commands = new()
+{
+    { "forward", (state, multiple) => (state.x + multiple, state.depth + state.aim * multiple, state.aim) },
+    { "up", (state, multiple) => (state.x, state.depth, state.aim - multiple) },
+    { "down", (state, multiple) => (state.x, state.depth, state.aim + multiple) },
+};
+
+(int x, int depth, int aim) position = (0, 0, 0);
+while (await Console.In.ReadLineAsync() is { } line)
+{
+    var input = line.Split(' ');
+    var command = commands[input[0]];
+    var multiple = int.Parse(input[1]);
+    position = command(position, multiple);
+}
+
+await Console.Out.WriteLineAsync((position.x * position.depth).ToString());
