1a660a325c5b4bb14da692205ae106eb27e60889
Martin Zikmund <martin@zikmund.dev>
2021-12-06 21:58:28 +0100

    Day 01

---
+++ Day01_1/Program.cs
@@ -0,0 +1,11 @@
+﻿int increases = 0;
+var previous = int.MaxValue;
+while (int.TryParse(await Console.In.ReadLineAsync(), out var current))
+{
+    if (current > previous)
+    {
+        increases++;
+    }
+    previous = current;
+}
+await Console.Out.WriteLineAsync(increases.ToString());
---
+++ Day01_2/Program.cs
@@ -0,0 +1,27 @@
+﻿int increases = 0;
+int[] window = new int[3];
+window[0] = ReadNumber();
+window[1] = ReadNumber();
+window[2] = ReadNumber();
+int windowEnd = 0;
+var previousSum = window.Sum();
+
+while (int.TryParse(await Console.In.ReadLineAsync(), out var currentNumber))
+{
+    var currentSum = previousSum - window[windowEnd] + currentNumber;
+    if (currentSum > previousSum)
+    {
+        increases++;
+    }
+    previousSum = currentSum;
+    window[windowEnd] = currentNumber;
+    windowEnd = (windowEnd + 1) % window.Length;
+}
+
+await Console.Out.WriteLineAsync(increases.ToString());
+
+int ReadNumber()
+{
+    return int.Parse(Console.In.ReadLine()!);
+}
+
