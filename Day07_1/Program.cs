8c7413da3f4aa48cbb1789be4852b229815e4902
Martin Zikmund <martin@zikmund.dev>
2021-12-07 18:44:15 +0100

    Day 7

---
+++ Day07_1/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = await Console.In.ReadLineAsync();
+var positions = input!.Split(',').Select(int.Parse).OrderBy(i => i).ToArray();
+var intervalStart = positions[positions.Length / 2];
+var intervalEnd = positions[positions.Length / 2 + 1];
+var bestCost = int.MaxValue;
+var bestPosition = 0;
+for (int i = intervalStart; i <= intervalEnd; i++)
+{
+    var cost = positions.Sum(p => Math.Abs(p - i));
+    if (cost < bestCost)
+    {
+        bestCost = cost;
+        bestPosition = i;
+    }
+}
+await Console.Out.WriteLineAsync(bestCost.ToString());
---
+++ Day07_2/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = await Console.In.ReadLineAsync();
+var positions = input!.Split(',').Select(int.Parse).ToArray();
+var intervalStart = (int)positions.Average();
+var intervalEnd = intervalStart + 1;
+var bestCost = int.MaxValue;
+var bestPosition = 0;
+for (int i = intervalStart; i <= intervalEnd; i++)
+{
+    var cost = positions.Sum(p => ArithmeticProgressionSum(Math.Abs(p - i)));
+    if (cost < bestCost)
+    {
+        bestCost = cost;
+        bestPosition = i;
+    }
+}
+await Console.Out.WriteLineAsync(bestCost.ToString());
+
+int ArithmeticProgressionSum(int max)
+{
+    return max * (max + 1) / 2;
+}
