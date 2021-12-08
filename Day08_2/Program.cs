739e7a06a2ada23470467a293837f65249f00a36
Martin Zikmund <martin@zikmund.dev>
2021-12-08 10:07:25 +0100

    Improved day 8

--- Day08_2/Program.cs
+++ Day08_2/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Globalization;
-
-long sum = 0;
+﻿long sum = 0;
 
 var digitSegments = new string[]
 {
@@ -23,65 +21,51 @@ while (await Console.In.ReadLineAsync() is { } line)
     var inputParts = line.Split('|');
     var digitList = inputParts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(d => d.Trim()).ToList();
     var outputList = inputParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(d => d.Trim()).ToList();
-    foreach (var permutation in GetPermutations(letters, letters.Length))
+
+    var number1 = digitList.Single(d => d.Length == 2);
+    var number7 = digitList.Single(d => d.Length == 3);
+    var number4 = digitList.Single(d => d.Length == 4);
+    var number8 = digitList.Single(d => d.Length == 7);
+    var number6 = digitList.Single(d => d.Length == 6 && d.Intersect(number1).Count() == 1);
+    var number0 = digitList.Single(d => d.Length == 6 && d.Intersect(number4).Count() == 3 && !d.SequenceEqual(number6));
+
+    var segmentA = number7.Except(number1).Single();
+    var segmentC = number1.Except(number6).Single();
+    var segmentF = number1.Except(new[] { segmentC }).Single();
+    var segmentB = number4.Intersect(number0).Except(new[] { segmentC, segmentF }).Single();
+    var segmentD = number4.Except(number0).Single();
+
+    var number3 = digitList.Single(d => d.Length == 5 && d.Intersect(new[] { segmentC, segmentD, segmentF }).Count() == 3);
+
+    var segmentG = number3.Except(number4).Except(number7).Single();
+    var segmentE = number8.Except(number4).Except(number3).Single();
+
+    var actualMapping = new char[] {
+        segmentA,
+        segmentB,
+        segmentC,
+        segmentD,
+        segmentE,
+        segmentF,
+        segmentG
+    };
+
+    var outputDigits = "";
+    foreach (var outputNumber in outputList)
     {
-        var permutationArray = permutation.ToArray();
-        var translatedDigits = new List<string>();
-
-        foreach (var digit in digitList)
+        var translated = new List<char>();
+        for (int i = 0; i < outputNumber.Length; i++)
         {
-            var digitCopy = "";
-            for (int i = 0; i < digit.Length; i++)
-            {
-                digitCopy += permutationArray[(int)(digit[i] - 'a')];
-            }
-
-            translatedDigits.Add(new string(digitCopy.OrderBy(c=>c).ToArray()));
+            var segmentIndex = Array.IndexOf(actualMapping.ToArray(), outputNumber[i]);
+            translated.Add(letters[segmentIndex]);
         }
 
-        bool isSolution = true;
-        foreach (var templateDigit in digitSegments)
-        {
-            if (!translatedDigits.Contains(templateDigit))
-            {
-                isSolution = false;
-                break;
-            }
-        }
+        var translatedString = new string(translated.OrderBy(c => c).ToArray());
 
-        if (isSolution)
-        {
-            // Translate output digits
-            var outputDigits = new List<string>();
-            foreach (var digit in outputList)
-            {
-                var digitCopy = "";
-                for (int i = 0; i < digit.Length; i++)
-                {
-                    digitCopy += permutationArray[(int)(digit[i] - 'a')];
-                }
-
-                outputDigits.Add(new string(digitCopy.OrderBy(c => c).ToArray()));
-            }
-
-            var currentValue = "";
-            foreach (var output in outputDigits)
-            {
-                currentValue += Array.IndexOf(digitSegments, output).ToString();
-            }
-            sum += int.Parse(currentValue);
-            break;
-        }
+        outputDigits += Array.IndexOf(digitSegments, translatedString).ToString();
     }
+
+    sum += int.Parse(outputDigits);
 }
 
 Console.Out.WriteLine(sum.ToString());
-
-static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
-{
-    if (length == 1) return list.Select(t => new T[] { t });
-
-    return GetPermutations(list, length - 1)
-        .SelectMany(t => list.Where(e => !t.Contains(e)),
-            (t1, t2) => t1.Concat(new T[] { t2 }));
-}
