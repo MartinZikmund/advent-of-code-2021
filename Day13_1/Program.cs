8aca4a6236dd0d30aa761580c5bf3765661aab51
Martin Zikmund <martin@zikmund.dev>
2021-12-13 18:46:03 +0100

    Day 13

---
+++ Day13_1/Program.cs
@@ -0,0 +1,69 @@
+﻿using Tools;
+
+var lines = InputTools.ReadAllLines();
+
+var dotPositions = (
+    from line in lines
+    where !line.StartsWith("fold") && line != ""
+    let split = line.Split(',')
+    select new Point(int.Parse(split[0]), int.Parse(split[1]))
+).ToArray();
+
+var folds = (
+    from line in lines
+    where line.StartsWith("fold")
+    select line.Substring("fold along ".Length)
+).ToArray();
+
+var currentWidth = dotPositions.Max(p => p.X) + 1;
+var currentHeight = dotPositions.Max(p => p.Y) + 1;
+
+var map = new bool[currentWidth, currentHeight];
+
+foreach (var position in dotPositions)
+{
+    map[position.X, position.Y] = true;
+}
+
+PerformFold(folds[0]);
+
+var counter = 0;
+for (int y = 0; y < currentHeight; y++)
+{
+    for (int x = 0; x < currentWidth; x++)
+    {
+        counter += map[x, y] ? 1 : 0;
+    }
+}
+
+Console.WriteLine(counter);
+
+void PerformFold(string fold)
+{
+    var foldInstruction = fold.Split('=');
+    var axis = foldInstruction[0][0];
+    var distance = int.Parse(foldInstruction[1]);
+
+    // Perform the fold
+    var startX = axis == 'x' ? distance : 0;
+    var startY = axis == 'y' ? distance : 0;
+
+    for (int y = startY; y < currentHeight; y++)
+    {
+        for (int x = startX; x < currentWidth; x++)
+        {
+            if (axis == 'x')
+            {
+                map[distance - (x - distance), y] |= map[x, y];
+            }
+            else
+            {
+                map[x, distance - (y - distance)] |= map[x, y];
+            }
+        }
+    }
+
+    // Adjust actual size
+    currentWidth = axis == 'x' ? distance : currentWidth;
+    currentHeight = axis == 'y' ? distance : currentHeight;
+}
