070be81724dc4cdef387cabaea564f2c074908ca
Martin Zikmund <martin@zikmund.dev>
2021-12-17 17:53:36 +0100

    Day 12

---
+++ Day12_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using Tools;
+
+const int Flashed = 100000;
+
+var lines = InputTools.ReadAllLines();
+
+Dictionary<string, Cave> caves = new();
+
+foreach (var line in lines)
+{
+    var connection = line.Split('-');
+    var cave1 = AddOrCreateCave(connection[0].Trim());
+    var cave2 = AddOrCreateCave(connection[1].Trim());
+    cave1.Connections.Add(cave2);
+    cave2.Connections.Add(cave1);
+}
+
+var start = caves["start"];
+var pathCount = GetPathCount(start, new List<string>() { "start" }, false);
+Console.WriteLine(pathCount);
+
+long GetPathCount(Cave current, List<string> visited, bool didDoubleVisit)
+{
+    if (current.Name == "end")
+    {
+        return 1;
+    }
+
+    var totalPaths = 0L;
+    foreach (var connection in current.Connections)
+    {
+        if (connection.Name == "start")
+        {
+            continue;
+        }
+
+        if (char.IsUpper(connection.Name[0]) || !visited.Contains(connection.Name) || !didDoubleVisit)
+        {
+            var secondVisit = didDoubleVisit || (char.IsLower(connection.Name[0]) && visited.Contains(connection.Name));
+            visited.Add(connection.Name);
+            totalPaths += GetPathCount(connection, visited, secondVisit);
+            visited.Remove(connection.Name);
+        }
+    }
+    return totalPaths;
+}
+
+Cave AddOrCreateCave(string name)
+{
+    if (!caves.TryGetValue(name, out var cave))
+    {
+        cave = new Cave(name);
+        caves.Add(name, cave);
+    }
+
+    return cave;
+}
+
+public class Cave
+{
+    public Cave(string name) => Name = name;
+
+    public string Name { get; }
+
+    public List<Cave> Connections { get; } = new List<Cave>();
+}
